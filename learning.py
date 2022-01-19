d8d433b77e714fa95cf8004e2252129c993a351a
graterbeeing <oz.braverman@gmail.com>
2022-01-19 08:36:36 +0200

    line tilte

--- learning.py
+++ learning.py
@@ -13,7 +13,24 @@ class Button:
 
     def draw(self):
         arcade.draw_rectangle_filled(self.point_x, self.point_y, self.width, self.hight, self.color)
+
+class Spin:
+    def __init__(self, spin_x, spin_y, width, hight, tilte, tilte_change, color):
+        self.spin_x= spin_x
+        self.spin_y= spin_y
+        self.width= width
+        self.hight=hight
+        self.color=color
+        self.tilte= tilte
+        self.tilte_change= tilte_change
     
+    def draw(self):
+        arcade.draw_rectangle_filled(self.spin_x, self.spin_y, self.width, self.hight, self.color, self.tilte)
+        #self.spin_x, self.spin_y, self.width, self.hight, self.tilte, self.color
+
+    def update(self):
+        self.tilte += self.tilte_change
+
 
 class My_window(arcade.Window):
     
@@ -22,14 +39,18 @@ class My_window(arcade.Window):
        arcade.set_background_color(arcade.color.ASH_GREY)
 
        self.button= Button(350,350, 100, 50, arcade.color.WHITE)
+       self.spin = Spin(550, 500, 10, 50, 10, 5, arcade.color.AMERICAN_ROSE)
+       #550, 500, 10, 50, arcade.color.AMERICAN_ROSE, 5, 50
 
     
     def on_draw(self):
         arcade.start_render()
         self.button.draw()
+        self.spin.draw()
 
     def update(self, delta_time):
-        pass
+        self.set_update_rate(1 / 20)
+        self.spin.update()
 
 
 
